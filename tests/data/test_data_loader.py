813d3e3dd27373300dd183e9eed451eaa676ddc8
Adem Alperen Arda <alperen.arda.adem22@gmail.com>
2025-08-05 12:25:18 +0300

    Add detailed explanations

--- tests/data/test_data_loader.py
+++ tests/data/test_data_loader.py
@@ -9,3 +9,8 @@ def test_check_if_file_extension_supported(data_loader):
     # Test with a supported file extension
     assert data_loader._check_if_file_extension_supported("data.csv") == ".csv"
     
+    with pytest.raises(TypeError):
+        data_loader._check_if_file_extension_supported("test.txt")
+
+# Çünkü _check_if_file_extension_supported("test.txt") fonksiyonu içinde TypeError fırlatılıyor, ValueError değil.
+# Her hata tipi farklıdır; pytest.raises(ValueError) yazsaydın hata tipi uyuşmadığı için test geçmezdi.
